84d86c5cba83a5adea2a484868bd00eee1e00817
Michal Pekar <pekac45@gmail.com>
2019-03-09 11:31:27 +0000

    clicky toggle hook

--- src/client/App.js
+++ src/client/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './app.css';
-import ReactImage from './react.png';
 
-class App extends Component {
-  state = { username: null };
+const App = () => {
+  const [isClicked, setClick] = useState(false);
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+  return (
+    <div>
+      <button onClick={() => setClick(!isClicked)}>Click Me!</button>
+      {isClicked && <h1>Hello</h1>}
+    </div>
+  );
+};
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   state = { username: null };
+
+//   componentDidMount() {
+//     fetch('/api/getUsername')
+//       .then(res => res.json())
+//       .then(user => this.setState({ username: user.username }));
+//   }
+
+//   render() {
+//     const { username } = this.state;
+//     return <div>{username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}</div>;
+//   }
+// }
 
 export default App;
