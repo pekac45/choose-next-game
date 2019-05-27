a6fe66427b4567385f1c027fc81ed58e6111892e
Michal Pekar <pekac45@gmail.com>
2019-05-27 23:13:56 +0100

    header ready

--- components/Header.js
+++ components/Header.js
@@ -3,39 +3,35 @@ import styled from "styled-components";
 import { Grid, Cell } from "styled-css-grid";
 
 const HeaderWrapper = styled.div`
-	border-bottom: 3px solid black;
+	border-bottom: 3px solid #113f67;
 	width: 100vw;
 	height: 50px;
-	position: relative;
 `;
 
 const LinkWrapper = styled.a`
-	position: relative;
-	color: purple;
-	top: 30%;
-
-	justify-self: center;
+	color: #113f67;
+	text-transform: uppercase;
 `;
 
 const Header = () => (
 	<HeaderWrapper>
-		<Grid>
-			<Cell>
+		<Grid rows={"50px"} gap={"0px"}>
+			<Cell center middle>
 				<Link href="/">
 					<LinkWrapper>Home</LinkWrapper>
 				</Link>
 			</Cell>
-			<Cell>
+			<Cell left={10} center middle>
 				<Link href="/Add">
-					<LinkWrapper left={10}>Add</LinkWrapper>
+					<LinkWrapper left={10}>Add Game</LinkWrapper>
 				</Link>
 			</Cell>
-			<Cell>
+			<Cell left={11} center middle>
 				<Link href="/Edit">
-					<LinkWrapper left={11}>Edit</LinkWrapper>
+					<LinkWrapper left={11}>Edit Game</LinkWrapper>
 				</Link>
 			</Cell>
-			<Cell left={12}>
+			<Cell left={12} center middle>
 				<Link href="/Logout">
 					<LinkWrapper>Logout</LinkWrapper>
 				</Link>
--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Header from "./Header";
 import styled, { createGlobalStyle } from "styled-components";
 
 const LayoutWrapper = styled.div`
-	color: red;
+	color: #113f67;
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -12,7 +12,12 @@ const GlobalStyle = createGlobalStyle`
 
 		@import url('https://fonts.googleapis.com/css?family=Dosis');
 		font-family: 'Dosis', sans-serif;
+
+		background-color: #f3f9fb;
+
+		text-align: center;
 	}
+
 `;
 
 const Layout = props => (
