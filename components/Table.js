ceaf63f1c51319fcaef19a181e406691b56b49f0
Michal Pekar <pekac45@gmail.com>
2019-06-01 16:02:01 +0100

    table stores gmaes in state

--- components/Table.js
+++ components/Table.js
@@ -1,33 +1,48 @@
-import { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDiceD20 } from "@fortawesome/free-solid-svg-icons";
-import { Grid, Cell } from "styled-css-grid";
-import { TableCell } from "../styles/components";
+import { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiceD20 } from '@fortawesome/free-solid-svg-icons';
+import { Grid, Cell } from 'styled-css-grid';
+import { TableCell } from '../styles/components';
 
 class GameRow extends Component {
-	render() {
-		const game = this.props.game;
-		return (
-			<tr>
-				<TableCell>{game.name}</TableCell>
-				<TableCell>
-					<FontAwesomeIcon icon={faDiceD20} />
-				</TableCell>
-			</tr>
-		);
-	}
+  render() {
+    const game = this.props.game;
+    return (
+      <tr>
+        <TableCell>{game.name}</TableCell>
+        <TableCell>
+          <FontAwesomeIcon icon={faDiceD20} />
+        </TableCell>
+      </tr>
+    );
+  }
 }
 
 class GameList extends Component {
-	render() {
-		const gameRows = this.props.games.map(game => <GameRow key={game.id} game={game} />);
-		return (
-			<div>
-				<table>
-					<tbody>{gameRows}</tbody>
-				</table>
-			</div>
-		);
-	}
+  constructor() {
+    super();
+    this.state = { games: [] };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({ games: this.props.games });
+    }, 2000);
+  }
+
+  render() {
+    const gameRows = this.state.games.map(game => <GameRow key={game.id} game={game} />);
+    return (
+      <div>
+        <table>
+          <tbody>{gameRows}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 export default GameList;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-import Layout from "../components/Layout";
-import GameList from "../components/Table";
-import { Grid, Cell } from "styled-css-grid";
+import Layout from '../components/Layout';
+import GameList from '../components/Table';
+import { Grid, Cell } from 'styled-css-grid';
 
 const games = [
-	{ id: 1, name: "Great Western Trail", hotness: 100, plays: 1 },
-	{ id: 2, name: "Terraforming Mars", hotness: 95, plays: 3 },
-	{ id: 3, name: "Spirit Island", hotness: 97, plays: 7 }
+  { id: 1, name: 'Great Western Trail', hotness: 100, plays: 1 },
+  { id: 2, name: 'Terraforming Mars', hotness: 95, plays: 3 },
+  { id: 3, name: 'Spirit Island', hotness: 97, plays: 7 }
 ];
 
 const Index = () => (
-	<Layout>
-		<Cell left={5} width={4} center>
-			<h1>Choose next game</h1>
-		</Cell>
-		<Cell left={2} width={8} top={3}>
-			<GameList games={games} />
-		</Cell>
-	</Layout>
+  <Layout>
+    <Cell left={5} width={4} center>
+      <h1>Choose next game</h1>
+    </Cell>
+    <Cell left={2} width={8} top={3}>
+      <GameList games={games} />
+    </Cell>
+  </Layout>
 );
 
 export default Index;
