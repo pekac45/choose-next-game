253e9042480b43ab52e4f903ea038e25b16a5af6
Michal Pekar <pekac45@gmail.com>
2019-06-23 02:21:08 +0100

    Add functionality where you can add existing games

--- pages/Add.js
+++ pages/Add.js
@@ -25,8 +25,49 @@ class Add extends Component {
     return gameName.length > 2;
   }
 
+  // TODO: MAKE THIS MORE DRY
+
   handleSubmit(e) {
-    // Make more input sanitizing
+    // TODO: Make more input sanitizing
+    e.preventDefault();
+
+    // Find out what is the current highest hottnes
+    fetch('http://localhost:3000/api/games')
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        // Make sure it works if there are no games in DB
+        let hotValue = () => {
+          if (data.length === 0) {
+            hotValue = 10000;
+          } else {
+            // eslint-disable-next-line prefer-destructuring
+            hotValue = _.orderBy(data, ['hotValue'], ['desc', 'asc'])[0].hotValue;
+          }
+        };
+        hotValue();
+
+        const form = document.forms.addGame;
+        const game = form.name.value;
+        const messageBody = JSON.stringify({
+          name: game,
+          hotValue
+        });
+
+        fetch(`http://localhost:3000/api/games`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: messageBody
+        });
+
+        form.name.value = '';
+      });
+  }
+
+  handleNewSubmit(e) {
     e.preventDefault();
 
     // Find out what is the current highest hottnes
@@ -75,7 +116,7 @@ class Add extends Component {
           <h2>Add new games</h2>
         </Cell>
         <Cell left={4} width={6} center top={3}>
-          <FormGroupWrapper name="addGame" onSubmit={this.handleSubmit}>
+          <FormGroupWrapper name="addGame">
             <InputWrapper
               type="text"
               name="name"
@@ -83,8 +124,11 @@ class Add extends Component {
               onChange={this.hangleGameNameChange}
               error={isError ? '0px 0px 2px 1px var(--danger-color)' : ''}
             />
-            <Button disabled={!isEnabled} className="btn">
-              Add Game
+            <Button disabled={!isEnabled} className="btn" onClick={this.handleNewSubmit}>
+              Add New
+            </Button>
+            <Button disabled={!isEnabled} className="btn" onClick={this.handleSubmit}>
+              Add Existing
             </Button>
           </FormGroupWrapper>
         </Cell>
